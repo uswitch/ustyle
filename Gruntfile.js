11daf315171e73758df5fb0cd5e4fd2646b93dca
David Annez <david.annez@uswitch.com>
2014-11-21 14:14:03 +0000

    Renamed styleguide

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,14 +4,29 @@ var _          = require('lodash');
 
 module.exports = function(grunt) {
   grunt.initConfig({
+    watch: {
+      files: "vendor/assets/stylesheets/ustyle/**/*.sass",
+      tasks: ['dss']
+    },
     dss: {
       docs: {
         files: {
           'docs/': 'vendor/assets/stylesheets/ustyle/**/*.sass'
         }
       }
+    },
+    browserSync: {
+        bsFiles: {
+            src : 'assets/css/*.css'
+        },
+        options: {
+            server: {
+                baseDir: "./build/"
+            }
+        }
     }
   });
+  grunt.loadNpmTasks('grunt-browser-sync');
   grunt.loadTasks('grunt/tasks');
   grunt.registerTask('default', ['dss']);
 };
