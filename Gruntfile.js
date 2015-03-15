657b3db84e1b5077e0d02b79877e1af68f0b819e
David Annez <david.annez@uswitch.com>
2015-03-15 15:47:32 +0000

    Fixing dependency

--- Gruntfile.js
+++ Gruntfile.js
@@ -151,7 +151,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-browser-sync');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-postcss');
   grunt.loadNpmTasks('grunt-newer');
