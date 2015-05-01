c5472cd6c95dc81030d1f3742dc9b543a94f19ef
David Annez <david.annez@uswitch.com>
2015-05-01 10:24:15 +0100

    Built ustyle from commit e806213 on branch master

--- js/app.js
+++ js/app.js
@@ -513,6 +513,67 @@ var hljs=new function(){function j(v){return v.replace(/&/gm,"&amp;").replace(/<
 
 }).call(this);
 
+(function() {
+  window.ClassToggler = (function() {
+    var defaults;
+
+    defaults = {
+      containerClass: null,
+      $target: null,
+      activeClass: "active",
+      inactiveClass: null,
+      toggleOn: 'click'
+    };
+
+    function ClassToggler(options) {
+      this.options = Utils.setOptions(options, defaults);
+      if (this.options.$target) {
+        this.addEventListeners();
+      } else {
+        console.trace("ClassToggle", this.options);
+      }
+    }
+
+    ClassToggler.prototype.addEventListeners = function() {
+      return this.options.$target.on(this.options.toggleOn, (function(_this) {
+        return function(e) {
+          var $togglableElement;
+          $togglableElement = _this.options.containerClass ? $(e.target).closest(_this.options.containerClass) : $(e.delegateTarget);
+          if (_this.isActive($togglableElement)) {
+            return _this.hide($togglableElement, e);
+          } else {
+            return _this.show($togglableElement, e);
+          }
+        };
+      })(this));
+    };
+
+    ClassToggler.prototype.isActive = function($togglableElement) {
+      return $togglableElement.hasClass(this.options.activeClass);
+    };
+
+    ClassToggler.prototype.show = function($togglableElement, e) {
+      var _base;
+      if (typeof (_base = this.options).onShow === "function") {
+        _base.onShow($togglableElement, e);
+      }
+      return $togglableElement.addClass(this.options.activeClass);
+    };
+
+    ClassToggler.prototype.hide = function($togglableElement, e) {
+      var _base;
+      if (typeof (_base = this.options).onHide === "function") {
+        _base.onHide($togglableElement, e);
+      }
+      return $togglableElement.removeClass(this.options.activeClass);
+    };
+
+    return ClassToggler;
+
+  })();
+
+}).call(this);
+
 (function(root, document, window){
   "use strict";
 
@@ -596,6 +657,12 @@ var hljs=new function(){function j(v){return v.replace(/&/gm,"&amp;").replace(/<
       )
     })
 
+    var tooltips = new ClassToggler({
+      containerClass: '.us-tooltip',
+      $target: $('.us-tooltip__icon'),
+      activeClass: "us-tooltip--active"
+    });
+    
     var tabs = new Tabs();
     var anchor = new Anchor({
       target: document.querySelector(".js-example-anchor"),
