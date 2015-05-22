4a4073dac90c7df4c19a44595a8635639005b2f8
David Annez <david.annez@uswitch.com>
2015-05-22 16:21:58 +0100

    Built ustyle from commit ccbb412 on branch master

--- js/app.js
+++ js/app.js
@@ -575,6 +575,39 @@
 
 }).call(this);
 
+(function() {
+  window.RadioToggle = (function() {
+    var defaults;
+
+    defaults = {
+      $target: $(".us-toggle")
+    };
+
+    function RadioToggle(options) {
+      this.options = Utils.setOptions(options, defaults);
+      if (this.options.$target) {
+        this.addEventListeners();
+      } else {
+        throw new Error("No target defined");
+      }
+    }
+
+    RadioToggle.prototype.addEventListeners = function() {
+      return this.options.$target.on("change", "input:radio", function(e) {
+        $("input[name=" + this.name + "]").removeClass("checked");
+        if (this.checked) {
+          $(this).addClass("checked");
+        }
+        return e.stopPropagation();
+      });
+    };
+
+    return RadioToggle;
+
+  })();
+
+}).call(this);
+
 function cleanWhiteSpace(codeBlocks){
     if (!codeBlocks) return;
 
@@ -645,7 +678,7 @@ function cleanWhiteSpace(codeBlocks){
     });
     
     var tabs = new Tabs();
-
+    var radio = new RadioToggle();
     var anchor = new Anchor({
       target: document.querySelector(".js-example-anchor"),
       content: document.querySelector(".js-example-anchor__target")
