3f8bdc87e5e46bc17f812a297ffae8b949899953
Ricardo <ricardomccerqueira@gmail.com>
2015-09-18 15:51:26 +0100

    password-helper jslint

--- vendor/assets/javascripts/ustyle/login/password-helper.js
+++ vendor/assets/javascripts/ustyle/login/password-helper.js
@@ -24,34 +24,40 @@ var slice = [].slice;
     function PasswordHelper(el, options) {
       this.tests = (this.options = $.extend({}, this.defaults, options)).tests;
       this.$el = $(el);
+
       if (this.$el.hasClass(this.options.classPrefix + "__input")) {
         return;
       }
+
       this.$el.addClass(this.options.classPrefix + "__input");
       this.wrapper = createWrapper(this.$el, this.options.classPrefix);
       this.hiddenPassword = createHiddenPassword(this.$el, this.options.classPrefix);
       this.strengthChecker();
+
       if (this.options.showHide) {
         this.showHide();
       }
     }
 
     PasswordHelper.prototype.strengthChecker = function() {
-      var html, strengthMeter, strengthTest;
-      html = "<div class='" + this.options.classPrefix + "__meter'>";
+      var html = "<div class='" + this.options.classPrefix + "__meter'>";
       html += "<div class='" + this.options.classPrefix + "__meter-bar' /></div>";
       this.$el.after($(html));
-      strengthMeter = $("." + this.options.classPrefix + "__meter-bar");
-      strengthTest = (function(_this) {
+      var strengthMeter = $("." + this.options.classPrefix + "__meter-bar");
+
+      var strengthTest = (function(_this) {
         return function(value) {
           var condition;
           if (!value.length) {
             return strengthMeter.removeClass();
           }
+
           condition = value.length < _this.options.minLength;
+
           if (condition) {
             return strengthMeter.removeClass().addClass("very-weak");
           }
+
           return $.each(_this.tests, function(name, test) {
             if (value.match(test)) {
               return strengthMeter.removeClass().addClass(name);
@@ -59,27 +65,30 @@ var slice = [].slice;
           });
         };
       })(this);
+
       return $(document).on('keyup', "." + this.options.classPrefix + "__input", (function(_this) {
         return function(e) {
           var passwordValue;
           passwordValue = $(e.target).val();
           $("." + _this.options.classPrefix + "__input").addClass("strength--started");
+
           if (!passwordValue.length) {
             $("." + _this.options.classPrefix + "__input").removeClass("strength--started");
           }
+
           return strengthTest(passwordValue);
         };
       })(this));
     };
 
     PasswordHelper.prototype.showHide = function() {
-      var exp, showClass;
-      exp = "<a class='" + this.options.classPrefix + "__show-hide' />";
+      var exp = "<a class='" + this.options.classPrefix + "__show-hide' />";
       this.$el.after($(exp).text(this.options.showText));
-      showClass = this.options.classPrefix + "__input--pass-shown";
+      var showClass = this.options.classPrefix + "__input--pass-shown";
       return $("." + this.options.classPrefix + "__show-hide").on("click", (function(_this) {
         return function(e) {
           e.preventDefault();
+
           if (!_this.$el.hasClass(showClass)) {
             _this.$el.addClass(showClass);
             _this.$el.prop('disabled', true).hide();
@@ -96,39 +105,40 @@ var slice = [].slice;
     };
 
     createHiddenPassword = function(el, classPrefix) {
-      var expression, input;
-      expression = "<input style='display: none' class='" + (el.attr('class')) + " ";
+      var expression = "<input style='display: none' class='" + (el.attr('class')) + " ";
       expression += classPrefix + "__input-hidden' type='text' ";
       expression += "name='" + (el.attr('name')) + "' ";
       expression += "placeholder='" + (el.attr('placeholder') || "") + "' ";
       expression += "size='" + (el.attr('size')) + "' value='' disabled='disabled' />";
-      input = $(expression);
+      var input = $(expression);
       el.after(input);
       return input;
     };
 
     createWrapper = function(el, classPrefix) {
-      var wrapper, wrapperCss;
-      wrapperCss = {
+      var wrapperCss = {
         position: 'relative',
         height: el.css('height'),
         display: el.css('display')
       };
-      return wrapper = el.wrap($("<div />").addClass(classPrefix + "__wrapper").css(wrapperCss));
+
+      return el.wrap($("<div />").addClass(classPrefix + "__wrapper").css(wrapperCss));
     };
 
     $.fn.extend({
       passwordHelper: function() {
-        var args, options;
-        options = arguments[0], args = 2 <= arguments.length ? slice.call(arguments, 1) : [];
+        var options = arguments[0]
+        var args = 2 <= arguments.length ? slice.call(arguments, 1) : [];
         return this.each(function() {
           var $this, data, helper;
           $this = $(this);
           data = $(this).data('passwordHelper');
+
           if (!data) {
             helper = (data = new PasswordHelper(this, options));
             $this.data('passwordHelper', helper);
           }
+
           if (typeof options === 'string') {
             return data[options].apply(data, args);
           }
@@ -137,6 +147,5 @@ var slice = [].slice;
     });
 
     return PasswordHelper;
-
   })();
 })(window.jQuery, window, document);
