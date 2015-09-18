f99d62dea78f3060c7f444e0f70d85d74c8e7a4e
Joe Green <joe.green@uswitch.com>
2015-09-18 10:39:10 +0100

    Remove version from bower.json and prevent the Grunt task for versioning from trying to increment it in the future.

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
     },
     version: {
       project: {
-        src: ['package.json', 'bower.json', 'lib/ustyle/version.rb']
+        src: ['package.json', 'lib/ustyle/version.rb']
       }
     },
     postcss: {
