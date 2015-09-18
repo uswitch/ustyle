ec8f09f198d7dd34cdd194f62ee59d510afbd21d
Ivo Reis <reis.ivo@gmail.com>
2015-09-18 16:01:22 +0100

    Added utils linting

--- vendor/assets/javascripts/ustyle/utils.js
+++ vendor/assets/javascripts/ustyle/utils.js
@@ -1,6 +1,13 @@
-var addClass, deleteUndefined, hasClass, merge, removeClass, requestAnimationFrame, setOptions, transformKey,
-  slice = [].slice,
-  hasProp = {}.hasOwnProperty;
+var addClass;
+var deleteUndefined;
+var hasClass;
+var merge;
+var removeClass;
+var requestAnimationFrame;
+var setOptions;
+var transformKey;
+var slice = [].slice;
+var hasProp = {}.hasOwnProperty;
 
 if (this.Utils == null) {
   this.Utils = {
@@ -14,18 +21,22 @@ addClass = function(element, name) {
 };
 
 removeClass = function(element, name) {
-  var regExp;
-  regExp = new RegExp("(\\s|^)" + name + "(\\s|$)", "gi");
+  var regExp = new RegExp("(\\s|^)" + name + "(\\s|$)", "gi");
   return element.className = element.className.replace(regExp, "");
 };
 
 hasClass = function(element, name) {
-  return new RegExp("(^| )" + name + "( |$)", 'gi').test(element.className);
+  return new RegExp("(^| )" + name + "( |$)", "gi").test(element.className);
 };
 
 merge = function() {
-  var extension, extensions, i, len, property, target;
-  target = arguments[0], extensions = 2 <= arguments.length ? slice.call(arguments, 1) : [];
+  var extension;
+  var extensions = 2 <= arguments.length ? slice.call(arguments, 1) : [];
+  var i;
+  var len;
+  var property;
+  var target = arguments[0];
+
   for (i = 0, len = extensions.length; i < len; i++) {
     extension = extensions[i];
     for (property in extension) {
@@ -33,6 +44,7 @@ merge = function() {
       target[property] = extension[property];
     }
   }
+
   return target;
 };
 
@@ -41,8 +53,9 @@ setOptions = function(options, defaults) {
 };
 
 deleteUndefined = function(obj) {
-  var key, results, value;
-  results = [];
+  var key;
+  var value;
+  var results = [];
   for (key in obj) {
     value = obj[key];
     if (value === null || value === void 0) {
@@ -51,13 +64,16 @@ deleteUndefined = function(obj) {
       results.push(void 0);
     }
   }
+
   return results;
 };
 
 transformKey = (function() {
-  var el, i, key, len, transforms;
-  el = document.createElement('div');
-  transforms = ['transform', 'webkitTransform', 'OTransform', 'MozTransform', 'msTransform'];
+  var i;
+  var key;
+  var len;
+  var el = document.createElement("div");
+  var transforms = ["transform", "webkitTransform", "OTransform", "MozTransform", "msTransform"];
   for (i = 0, len = transforms.length; i < len; i++) {
     key = transforms[i];
     if (el.style[key] !== void 0) {
@@ -67,15 +83,20 @@ transformKey = (function() {
 })();
 
 requestAnimationFrame = (function(window) {
-  var i, len, ref, vendor;
-  ref = ['ms', 'moz', 'webkit', 'o'];
+  var i;
+  var len;
+  var vendor;
+  var ref =  ["ms", "moz", "webkit", "o"];
+
   for (i = 0, len = ref.length; i < len; i++) {
     vendor = ref[i];
     if (window.requestAnimationFrame) {
       break;
     }
+
     window.requestAnimationFrame = window[vendor + "RequestAnimationFrame"];
   }
+
   return window.requestAnimationFrame || (window.requestAnimationFrame = function(callback) {
     return setTimeout(callback, 1000 / 60);
   });
