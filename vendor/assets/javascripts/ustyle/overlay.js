6c5d26a416e4613e247448c9f2ec6ccee0ff830b
Ivo Reis <reis.ivo@gmail.com>
2015-09-18 14:47:47 +0100

    Added linting to overlay

--- vendor/assets/javascripts/ustyle/overlay.js
+++ vendor/assets/javascripts/ustyle/overlay.js
@@ -1,18 +1,19 @@
-var addClass, hasClass, ref, removeClass, requestAnimationFrame, setOptions;
-
-ref = this.Utils, setOptions = ref.setOptions, hasClass = ref.hasClass, addClass = ref.addClass, removeClass = ref.removeClass, requestAnimationFrame = ref.requestAnimationFrame;
+var ref = this.Utils;
+var addClass = ref.addClass;
+var hasClass = ref.hasClass;
+var removeClass = ref.removeClass;
+var setOptions = ref.setOptions;
+var requestAnimationFrame = ref.requestAnimationFrame;
 
 window.Overlay = (function() {
-  var defaults;
-
-  defaults = {
-    bodyActiveClass: 'overlay--open',
-    activeClass: 'us-overlay-parent--active',
-    visibleClass: 'us-overlay-parent--visible',
-    overlay: $('.us-overlay-parent'),
-    openButton: '.js-open-overlay',
-    closeButton: '.js-close-overlay',
-    historyStatus: '#seedeal',
+  var defaults = {
+    bodyActiveClass: "overlay--open",
+    activeClass: "us-overlay-parent--active",
+    visibleClass: "us-overlay-parent--visible",
+    overlay: $(".us-overlay-parent"),
+    openButton: ".js-open-overlay",
+    closeButton: ".js-close-overlay",
+    historyStatus: "#seedeal",
     history: false,
     preventDefault: true,
     animationSpeed: 300
@@ -29,17 +30,24 @@ window.Overlay = (function() {
   }
 
   Overlay.prototype.addEventListeners = function() {
-    $(this.options.openButton).on('click.open-overlay', (function(_this) {
+    $(this.options.openButton).on("click.open-overlay", (function(_this) {
       return function(e) {
         if (_this.options.preventDefault) {
           e.preventDefault();
         }
+
         return _this.show(e);
       };
     })(this));
-    this.overlay.on('click.close-overlay', (function(_this) {
+
+    this.overlay.on("click.close-overlay", (function(_this) {
       return function(e) {
-        var i, len, results, target, targets;
+        var i;
+        var len;
+        var results;
+        var target;
+        var targets;
+
         targets = [_this.overlay[0], _this.overlay.find(_this.options.closeButton)[0]];
         results = [];
         for (i = 0, len = targets.length; i < len; i++) {
@@ -48,15 +56,18 @@ window.Overlay = (function() {
             if (_this.options.preventDefault) {
               e.preventDefault();
             }
+
             _this.hide(e);
             break;
           } else {
             results.push(void 0);
           }
         }
+
         return results;
       };
     })(this));
+
     if (this.hasHistory()) {
       return window.onpopstate = (function(_this) {
         return function(e) {
@@ -66,46 +77,52 @@ window.Overlay = (function() {
         };
       })(this);
     }
+
   };
 
   Overlay.prototype.show = function(e) {
-    var onFrame, that;
-    that = this;
+    var onFrame;
+    var _this = this;
     $(document.body).addClass(this.options.bodyActiveClass);
     this.backdrop.retain();
     addClass(this.overlay[0], this.options.visibleClass);
     onFrame = function() {
-      addClass(that.overlay[0], that.options.activeClass);
+      addClass(_this.overlay[0], _this.options.activeClass);
       return setTimeout(function() {
         var base;
-        return typeof (base = that.options).onOpen === "function" ? base.onOpen(e) : void 0;
-      }, that.options.animationSpeed);
+        return typeof (base = _this.options).onOpen === "function" ? base.onOpen(e) : void 0;
+      }, _this.options.animationSpeed);
     };
+
     requestAnimationFrame.call(window, onFrame);
     if (this.hasHistory()) {
-      return history.pushState('open', window.document.title, this.options.historyStatus);
+      return history.pushState("open", window.document.title, this.options.historyStatus);
     }
+
   };
 
   Overlay.prototype.hide = function(e) {
-    var onFrame, that;
-    that = this;
+    var onFrame;
+    var _this = this;
     $(document.body).removeClass(this.options.bodyActiveClass);
     this.backdrop.release();
     onFrame = function() {
-      removeClass(that.overlay[0], that.options.activeClass);
+      removeClass(_this.overlay[0], _this.options.activeClass);
       return setTimeout(function() {
         var base;
-        removeClass(that.overlay[0], that.options.visibleClass);
-        return typeof (base = that.options).onClose === "function" ? base.onClose(e) : void 0;
-      }, that.options.animationSpeed);
+        removeClass(_this.overlay[0], _this.options.visibleClass);
+        return typeof (base = _this.options).onClose === "function" ? base.onClose(e) : void 0;
+      }, _this.options.animationSpeed);
     };
+
     requestAnimationFrame.call(window, onFrame);
     if (this.hasHistory()) {
-      if (history.state === 'open') {
+      if (history.state === "open") {
         return history.back();
       }
+
     }
+
   };
 
   Overlay.prototype.isOpen = function() {
